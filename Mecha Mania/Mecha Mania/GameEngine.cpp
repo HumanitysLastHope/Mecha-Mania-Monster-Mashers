0e59f14e3bb956a3c7147faa573b6aa506808a62
Lance Chaney <lance.e.chaney@gmail.com>
2017-05-07 11:20:18 +1200

    Added framework for games state management.

---
+++ Mecha Mania/Mecha Mania/GameEngine.cpp
@@ -0,0 +1,24 @@
+#include "GameEngine.h"
+
+
+
+CGameEngine::CGameEngine()
+{
+}
+
+
+CGameEngine::~CGameEngine()
+{
+}
+
+void CGameEngine::Step()
+{
+}
+
+void CGameEngine::Draw()
+{
+}
+
+void CGameEngine::ChangeState(IGameState * _state)
+{
+}
