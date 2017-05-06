5b6924bbd0dec3c7ef85c0d950621d92f897e98a
jack.mai7246 <jwjmair@gmail.com>
2017-05-07 11:52:56 +1200

    edited Tiles

--- Mecha Mania/Mecha Mania/Tile.cpp
+++ Mecha Mania/Mecha Mania/Tile.cpp
@@ -11,12 +11,23 @@ CTile::~CTile()
 {
 }
 
-EENVIRONMENT CTile::GetEnviroment()
+EENVIRONMENT* CTile::GetEnvironment()
 {
 	return m_peEnvironment;
 }
 
+void  CTile::SetEnvironment(EENVIRONMENT* _peEnvironment)
+{
+	m_peEnvironment = _peEnvironment;
+}
+
+
 CMecha* CTile::GetMecha()
 {
 	return m_pMecha;
+}
+
+void  CTile::SetMecha(CMecha* _pMecha)
+{
+	m_pMecha = _pMecha;
 }
