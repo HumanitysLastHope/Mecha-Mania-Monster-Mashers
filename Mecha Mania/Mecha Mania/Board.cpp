d64a55ce7f7a7d0995965b7196123e564afdcfb5
jack.mai7246 <jwjmair@gmail.com>
2017-05-07 12:17:54 +1200

    Updated Board

--- Mecha Mania/Mecha Mania/Board.cpp
+++ Mecha Mania/Mecha Mania/Board.cpp
@@ -14,3 +14,7 @@ CBoard::~CBoard()
 CTile& CBoard:: GetTile(int _iX, int _iY) {
 	return m_pMap[_iX][_iY];
 }
+
+void CBoard::SetTile(int _iX, int _iY, CTile _pTile) {
+	m_pMap[_iX][_iY] = _pTile;
+}
