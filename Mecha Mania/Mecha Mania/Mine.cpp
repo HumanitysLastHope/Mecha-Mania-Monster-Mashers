176329e7eac5d82314b5d5d2a5345f2406634574
sebastian.ten7230 <sebastian.ten7230@1808C003>
2017-05-10 12:23:10 +1200

    Mines can now move

--- Mecha Mania/Mecha Mania/Mine.cpp
+++ Mecha Mania/Mecha Mania/Mine.cpp
@@ -1,8 +1,10 @@
 #include "Mine.h"
+#include "Board.h"
 
 
 
-CMine::CMine()
+CMine::CMine(CBoard* _pBoard, TPosition _posGridPosition):
+CMovable(_pBoard, _posGridPosition)
 {
 }
 
@@ -13,7 +15,7 @@ CMine::~CMine()
 
 TPosition CMine::getPosition()
 {
-	return m_gridPosition;
+	return m_posGridPosition;
 }
 
 void CMine::ArmMine()
@@ -24,6 +26,85 @@ void CMine::ArmMine()
 bool CMine::isArmed()
 {
 	return m_isArmed;
+}
+
+bool CMine::Move(EDIRECTION _eDirection)
+{
+	switch (_eDirection)
+	{
+	case WEST:
+		// Checks if you would hit a wall
+		if (m_posGridPosition.m_iX == 0)
+			return false;
+		// If there's a mine in front move that player
+		if (m_pBoard->GetTile(m_posGridPosition.m_iX - 1, m_posGridPosition.m_iY).GetMine() != nullptr)
+		{
+
+			if (m_pBoard->GetTile(m_posGridPosition.m_iX - 1, m_posGridPosition.m_iY).GetMine()->Move(_eDirection) == false)
+			{
+				return false;
+			}
+		}
+
+		m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY).SetMine(nullptr);
+		m_pBoard->GetTile(m_posGridPosition.m_iX - 1, m_posGridPosition.m_iY).SetMine(this);
+		m_posGridPosition.m_iX--;
+		break;
+
+	case NORTH:
+		if (m_posGridPosition.m_iY == 0)
+			return false;
+
+		if (m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY - 1).GetMine() != nullptr)
+		{
+			if (m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY - 1).GetMine()->Move(_eDirection) == false)
+			{
+				return false;
+			}
+		}
+
+		m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY).SetMine(nullptr);
+		m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY - 1).SetMine(this);
+		m_posGridPosition.m_iY--;
+		break;
+
+	case EAST:
+		if (m_posGridPosition.m_iX == 9)
+			return false;
+
+		if (m_pBoard->GetTile(m_posGridPosition.m_iX + 1, m_posGridPosition.m_iY).GetMine() != nullptr)
+		{
+			if (m_pBoard->GetTile(m_posGridPosition.m_iX + 1, m_posGridPosition.m_iY).GetMine()->Move(_eDirection) == false)
+			{
+				return false;
+			}
+		}
+
+		m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY).SetMine(nullptr);
+		m_pBoard->GetTile(m_posGridPosition.m_iX + 1, m_posGridPosition.m_iY).SetMine(this);
+		m_posGridPosition.m_iX++;
+		break;
+
+	case SOUTH:
+		if (m_posGridPosition.m_iY == 9)
+			return false;
+
+		if (m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY + 1).GetMine() != nullptr)
+		{
+			if (m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY + 1).GetMine()->Move(_eDirection) == false)
+			{
+				return false;
+			}
+		}
+		m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY).SetMine(nullptr);
+		m_pBoard->GetTile(m_posGridPosition.m_iX, m_posGridPosition.m_iY + 1).SetMine(this);
+		m_posGridPosition.m_iY++;
+		break;
+
+	default:
+		break;
+	}
+	return true;
 }
 
 void CMine::bombBlown()
